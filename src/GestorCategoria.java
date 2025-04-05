82a94aab7260d257dee8327fc0850b61151ef7c6
Felipe Arosti <118208364+felipearosti@users.noreply.github.com>
2025-04-05 00:55:18 -0300

    Att Remover Categoria

--- src/GestorCategoria.java
+++ src/GestorCategoria.java
@@ -8,7 +8,7 @@ public class GestorCategoria {
         this.categorias = new ArrayList<>();
     }
 
-    public List<Categoria> listarCategorias() {               // Lista categoria              
+    public List<Categoria> listarCategorias() {                 
         return categorias;
     }
     
@@ -16,5 +16,9 @@ public class GestorCategoria {
         Categoria categoria = new Categoria(nome, tipo);                          // Adiciona categoria nova
         categorias.add(categoria);                                                    
         System.out.println("Categoria '" + nome + "' adicionada com sucesso!");
+    }
+
+    public void removerCategoria(String nome) {
+        categorias.removeIf(categoria -> categoria.getNome().equals(nome));         // Remove uma categoria
     }
 }
