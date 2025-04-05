25cd516e3c294fd72c84cbb32c4469c5c65ece59
Felipe Arosti <118208364+felipearosti@users.noreply.github.com>
2025-04-05 12:06:51 -0300

    Update GerenciarCategoriasGUI.java
    Colocando janela, lista, botões e dando init na interface

--- src/GerenciarCategoriasGUI.java
+++ src/GerenciarCategoriasGUI.java
@@ -1,5 +1,13 @@
-public class GerenciarCategoriasGUI extends JFrame {
-
+public class GerenciarCategoriasGUI {
+    private JFrame frame;                            //Janela
+    private JList<String> listaCategorias;
+    private JButton btnAdicionar, btnEditar, btnRemover;        //Botoes
     private GestorCategoria gestorCategoria;
 
+
+    public GerenciarCategoriasGUI(GestorCategoria gestorCategoria) {
+        this.gestorCategoria = gestorCategoria;
+        initalize(); 
+
+    }
 }
