43bce0471f82e2d14faff1801dcead79bff331b9
Felipe Arosti <118208364+felipearosti@users.noreply.github.com>
2025-04-04 23:58:14 -0300

    Classe Categorias - Atualização
    Trocar arquivo de categorias

--- src/Categoria.java
+++ src/Categoria.java
@@ -1,31 +1,33 @@
 public class Categoria {
-    //atributos
     private String nome;
-    private String tipo;
+    private TipoCategoria tipo;
 
-    //construtores
-    public Categoria(String nome, String tipo){
+    // Construtor
+    public Categoria(String nome, TipoCategoria tipo) {
         this.nome = nome;
         this.tipo = tipo;
     }
-    public Categoria(){
 
-    }
-
-    //métodos
-
+    // Métodos Getters e Setters
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getTipo() {
+    public TipoCategoria getTipo() {
         return tipo;
     }
-    public void setTipo(String tipo) {
+
+    public void setTipo(TipoCategoria tipo) {
         this.tipo = tipo;
     }
 
+    // Sobrescreve o metodo toString para exibir o nome da categoria
+    @Override
+    public String toString() {
+        return nome; // Retorna o nome da categoria para exibição
+    }
 }
